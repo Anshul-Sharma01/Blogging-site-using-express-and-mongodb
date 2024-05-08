078422ad3e3eb6bce75dce40f72019804d8f2d4f
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-05-08 23:39:12 +0530

    Some issue occrring with home.ejs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const session = require("express-session");
 const mongodb = require("mongodb");
 const multer = require("multer");
-
+const fs = require("fs");
 
 
 
@@ -60,6 +60,8 @@ function authorization(req, res, next){
         res.redirect("/home");
     }
 }
+
+// Singin an login
 
 app.get("/login", (req,res) => {
     if(req.session.userAuthenticated){
@@ -124,31 +126,45 @@ app.post("/signin", (req, res) => {
     })
 })
 
-app.get("/home", authentication, (req,res) => {
-    console.log(req.session.userData);
-    const userRole = req.session.userData.role;
-    if(req.session.userData.role == "admin"){
-        dbinstance.collection("blogs").find({}).toArray().then((data) => {
-            console.log("Data sent for admin...");
-            res.render("home",{message:`Welcome ${req.session.userData.name}`,userblogs:data,userRole});
-        }).catch((err) => {
-            console.error("Error in sending data for admin : ",err);
-        })
-    }else{
-        const username = req.session.userData.username;
-        dbinstance.collection("blogs").findOne({username}).then((data) => {
-            console.log("Data sent for user...");
-            res.render("home",{message:`Welcome ${req.session.userData.name}`,userblogs:data,userRole});
-        }).catch((err) => {
-            console.error("Error in sending data for author");
-        })
-    }
-})
+
+// home
+
+
+// app.get("/home", authentication, (req, res) => {
+//     const userRole = req.session.userData.role;
+//     const username = req.session.userData.username;
+
+//     if (userRole == "author") {
+//         dbinstance.collection("blogs").findOne({ username: username }).then((userblogs) => {
+//             if (!userblogs || !userblogs.blogs) {
+//                 // Handle case when user has no blogs
+//                 res.render("home", { message: `Welcome ${req.session.userData.name}`, userblogs: null, userRole });
+//             } else {
+//                 // User has blogs
+//                 res.render("home", { message: `Welcome ${req.session.userData.name}`, userblogs: userblogs, userRole });
+//             }
+//         }).catch((err) => {
+//             console.error("Error in sending data for author: ", err);
+//         });
+//     } else if (userRole == "admin") {
+//         // Admin fetching all blogs
+//         dbinstance.collection("blogs").find({}).toArray().then((allBlogs) => {
+//             res.render("home", { message: `Welcome ${req.session.userData.name}`, userblogs: { blogs: allBlogs }, userRole });
+//         }).catch((err) => {
+//             console.error("Error in sending data for admin: ", err);
+//         });
+//     }
+// });
+
 
 app.get("/",(req,res) => {
     console.log(req.url);
     res.send("<h1>This is a trial webpage</h1>");
 })
+
+
+// Create-Post
+
 
 app.get("/createpost", authentication, (req, res) => {
     res.render("createPost");
@@ -156,18 +172,178 @@ app.get("/createpost", authentication, (req, res) => {
 
 app.post("/createpost", upload.single("thumbnail"), (req, res) => {
     const { title, content } = req.body;
-    const filepath = req.file.filename;
+    const filename = req.file.filename;
+    const userRole = req.session.userData.role;
     const uname = req.session.userData.username;
     const newBlog = {
-        title, content, filepath, created_at:new Date()
+        title,
+        content,
+        filename,
+        created_at: new Date()
     };
-    dbinstance.collection("blogs").updateOne({username:uname}, {$push : { blogs : newBlog }}).then((data) => {
-        console.log("Blog post created and added to the user's blogs array.");
+
+    dbinstance.collection("blogs").findOne({ username: uname })
+        .then((data) => {
+            if (data) {
+                dbinstance.collection("blogs").updateOne(
+                    { username: uname },
+                    { $push: { blogs: newBlog } }
+                );
+            } else {
+                const newUserBlogs = {
+                    username: uname,
+                    role:userRole,
+                    blogs: [newBlog]
+                };
+                dbinstance.collection("blogs").insertOne(newUserBlogs);
+            }
+        })
+        .then(() => {
+            console.log("Blog post created and added to the user's blogs array.");
+            res.redirect("/home");
+        })
+        .catch((err) => {
+            console.error("Error updating user document with new blog post:", err);
+            res.redirect("/home");
+        });
+});
+
+
+// Update-Post
+
+
+
+app.get("/updatepost/:blogid", authentication, (req, res) => {
+    const blogind = req.params.blogid;
+    const username = req.session.userData.username;
+    dbinstance.collection("blogs").findOne({username}).then((data) => {
+        if (!data || !data.blogs || !data.blogs[blogind]) {
+            console.error("Data or blog not found for update.");
+            res.redirect("/home");
+            return;
+        }
+
+        res.render("updatePost", { data: data.blogs[blogind], blogind });
+    }).catch((err) => {
+        console.error("Error in finding the blog data: ", err);
+        res.redirect("/home");
+    });
+});
+
+app.post("/updatepost/:blogid", upload.single("thumbnail"), (req, res) => {
+    const blogId = parseInt(req.params.blogid, 10); // Convert blogId to integer
+    const { title, content } = req.body;
+    const username = req.session.userData.username;
+    const newFilename = req.file ? req.file.filename : null; // Get the new filename if provided
+
+    // Find the user and blog post by username
+    dbinstance.collection("blogs").findOne({ username }).then((data) => {
+        if (!data || !Array.isArray(data.blogs) || data.blogs.length <= blogId) {
+            console.log("Blog post not found.");
+            return res.redirect("/home");
+        }
+
+        // Retrieve the blog post data
+        const blog = data.blogs[blogId];
+        const prevFilename = blog.filename;
+
+        // Update the blog post with the new details
+        blog.title = title;
+        blog.content = content;
+        blog.updated_at = new Date();
+
+        // Update the filename and delete the previous file if a new thumbnail is provided
+        if (newFilename) {
+            // Delete the previous file if it exists
+            if (prevFilename) {
+                const prevFilePath = `./uploads/${prevFilename}`;
+                fs.unlink(prevFilePath, (err) => {
+                    if (err) {
+                        console.error("Error deleting previous photo:", err);
+                    } else {
+                        console.log("Previous photo deleted successfully.");
+                    }
+                });
+            }
+            // Set the new filename in the blog data
+            blog.filename = newFilename;
+        }
+
+        // Update the database
+        dbinstance.collection("blogs").updateOne(
+            { _id: data._id, username: data.username },
+            { $set: { [`blogs.${blogId}`]: blog } }
+        )
+        .then(() => {
+            console.log("Blog post updated successfully.");
+            // Redirect the user to the /home endpoint
+            res.redirect("/home");
+        })
+        .catch((err) => {
+            console.log("Error updating the blog post:", err);
+            res.redirect("/home");
+        });
+    })
+    .catch((err) => {
+        console.log("Error finding the blog data:", err);
+        res.redirect("/home");
+    });
+});
+
+
+
+// Update-Info
+
+app.get("/updateinfo", authentication, (req, res) => {
+    const  userName = req.session.userData.username; 
+    dbinstance.collection("users").findOne({username:userName}).then((data) => {
+        console.log("Data found for updation..");
+        res.render("updateInfo",{userData:data});
+    }).catch((err) => {
+        console.log("Error occurred while finding the user for updation");
+    })
+})
+
+app.post("/updateinfo", (req, res) => {
+    const prevuname = req.session.userData.username;
+    const { username, name, password } = req.body;
+    if(req.session.userData.role == "admin"){
+        userRole = "admin";
+    }else{
+        userRole = "author";
+    }
+    const updatedData = {
+        username,name,password,role:userRole
+    }
+    dbinstance.collection("users").updateOne({ username : prevuname }, { $set : updatedData}).then((data) => {
+        if(data){
+            console.log("User data successfully updated");
+            req.session.userData.username = updatedData.username;
+            req.session.userData.name = updatedData.name;
+        }else{
+            console.log("user not updated..");
+            res.redirect("/home");
+        }
+    }).catch((err) => {
+        console.log("Error in updating the user info");
+        res.redirect("/home");
+    })
+
+    dbinstance.collection("blogs").updateOne({username:prevuname},{$set : {username: updatedData.username}}).then((data) => {
+        console.log("User also updated in blogs..");
         res.redirect("/home");
     }).catch((err) => {
-        console.log("Error updating user document with new blog post: ", err);
-    });
+        console.log("Error in updating the user in the blogs db");
+        res.redirect("/home");
+    })
+})
 
+
+// Logout
+
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("/login");
 })
 
 app.listen(5000, (err) => {
